7c1c477f5d5d9b0c8fbf595e6a02e1adbe7a3f46
unknown <rohithasatya314@gmail.com>
2021-07-05 07:31:00 +0530

    Modified:Max in float

--- FindMaximumNumber.java
+++ FindMaximumNumber.java
@@ -13,11 +13,26 @@ public class FindMaximumNumber {
 		}
 		return max;
 	}
+	public static Double checkDoubleMaximum(Double[] x)
+	{
+		Double max=x[0];
+		for(Double e:x)
+		{
+			if(e.compareTo(max)>0)
+			{
+				max=e;
+			}
+		}
+		return max;
+		
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome");
 		Integer arr[]= {7,2,3};
-	    System.out.println("Maximum number in integer array is "+checkIntMaximum(arr));		
-	}	
+	    System.out.println("Maximum number in integer array is "+checkIntMaximum(arr));
+	    Double arr1[]= {1.2,2.9,3.4};
+	    System.out.println("Maximum number in double array is "+checkDoubleMaximum(arr1));  
+	}
 
 }
