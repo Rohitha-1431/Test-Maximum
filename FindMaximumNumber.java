8c5c83578d85fa0b92bbbed1adaa3f1d22608ab4
unknown <rohithasatya314@gmail.com>
2021-07-05 07:25:27 +0530

    Modified:max num in integer

--- FindMaximumNumber.java
+++ FindMaximumNumber.java
@@ -1,7 +1,23 @@
 package com.bridgelabz.testmaxium;
 
 public class FindMaximumNumber {
-	public static void main(String[] args) {
-	System.out.println("Welcome to Find Maximum Number");
+	public static Integer checkIntMaximum(Integer[] x)
+	{
+		Integer max=x[0];
+		for(Integer e:x)
+		{
+			if(e.compareTo(max)>0)
+			{
+				max=e;
+			}
+		}
+		return max;
 	}
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome");
+		Integer arr[]= {7,2,3};
+	    System.out.println("Maximum number in integer array is "+checkIntMaximum(arr));		
+	}	
+
 }
