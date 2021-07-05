00fcb19f92dce3fe65c908f26b82652e56369743
unknown <rohithasatya314@gmail.com>
2021-07-05 07:35:44 +0530

    modified:max in strings

--- FindMaximumNumber.java
+++ FindMaximumNumber.java
@@ -24,7 +24,18 @@ public class FindMaximumNumber {
 			}
 		}
 		return max;
-		
+	}
+	private static String checkStringMaximum(String[] x)
+	{
+		String max=x[0];
+		for(String e:x)
+		{
+			if(e.compareTo(max)>0)
+			{
+				max=e;
+			}
+		}
+		return max;
 	}
 	public static void main(String[] args)
 	{
@@ -32,7 +43,9 @@ public class FindMaximumNumber {
 		Integer arr[]= {7,2,3};
 	    System.out.println("Maximum number in integer array is "+checkIntMaximum(arr));
 	    Double arr1[]= {1.2,2.9,3.4};
-	    System.out.println("Maximum number in double array is "+checkDoubleMaximum(arr1));  
+	    System.out.println("Maximum number in double array is "+checkDoubleMaximum(arr1));
+	    String arr2[]= {"Apple","Peach","Banana"};
+	    System.out.println("Maximum in String array is "+checkStringMaximum(arr2));
 	}
 
 }
